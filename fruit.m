82f160d56c62c3e0f3d113f242baeddc9db53338
Agroth1 <agroth439@gmail.com>
2024-11-08 18:28:18 -0600

    Answer

--- fruit.m
+++ fruit.m
@@ -1,7 +1,19 @@
-function [] = fruit( N )
 % This function prints for values from 1 to N:
 % “apple” if the value is divisible by 2
 % “banana” if the value is divisible by 5
 % The number itself if neither is true
 % “applebanana” if both are true
 
+function [] = fruit(N)
+    for i = 1:N
+        if mod(i,2) == 0 && mod(i,5) == 0
+            fprintf('applebanana\n')
+        elseif mod(i,2) == 0
+            fprintf('apple\n')
+        elseif mod(i,5) == 0
+            fprintf('banana\n')
+        else
+            fprintf('%d\n', i)
+        end             
+    end
+end
